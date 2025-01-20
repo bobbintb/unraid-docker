d934a7e154e5b451b0a9703bbdfad9fa0ad2d576
bobbintb <bobbintb@gmail.com>
2025-01-20 14:10:58 -0700

    Update bzroot.sh

--- bzroot.sh
+++ bzroot.sh
@@ -1,7 +1,15 @@
 . ./download.sh
 
 ##### bzroot #####
+unzip "$filename" "bzroot" -d "$dir"
+cd ./"$dir" || exit
+mkdir root
+root=$(realpath ./root)
 cp bzroot "$root"
 cd "$root" || exit
 lsinitrd --unpack bzroot
+rm bzroot
+rm ../bzroot
 cd ..
+tar -cf bzroot.tar -C ./root .
+mv bzroot.tar ../bzroot/
